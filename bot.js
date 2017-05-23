555c75de9f4cebc557007f47e33729d7d76e6918
Bit <BitNetwork@users.noreply.github.com>
2017-05-22 22:09:50 -0600

    token redacted (no longer valid anyways) & cleanup

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ var logg = chalk.bold.cyan;
 const client = new Discord.Client();
 
 // The token of your bot - https://discordapp.com/developers/applications/me
-const token = 'MzE1MjQ0NzA0NTkxNTExNTUy.DAQ7qA.qG5jnpNNMDkpcCswctOg3PNYGnw';
+const token = '[redacted]';
 
 // The ready event is vital, it means that your bot will only start reacting to information
 // from Discord _after_ ready is emitted
@@ -28,50 +28,25 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    if (message.content === '#help') {
-      message.channel.send('**SatanicBot Version 0.1.0**')
-      message.channel.send(' ')
-      message.channel.send("**Test**")
-      message.channel.send('#ping - Sends A Pong Message')
-      message.channel.send('**Fun**')
-      message.channel.send('#hello - Sends Hello Comrade')
-      message.channel.send('**Contact/Support**')
-      message.channel.send('#official - Sends Links To Invite And Official Discord Server')
+  if (message.content === '#help') {
+    message.channel.send('**SatanicBot Version 0.1.0**\n\n**Test**\n#ping - Sends A Pong Message\n**Fun**\n#hello - Sends Hello Comrade\n**Contact/Support**\n#official - Sends Links To Invite And Official Discord Server');
   }
-});
 
-client.on('message', message=> {
-    if (message.content === '#log') {
-        log = !log;     
-            
-     } else if (log === true) {
-         console.log(chalk.green(message.guild.name) + chalk.grey(":") + logg(message.channel.name) + chalk.grey(":") + chalk.blue(message.author.tag) + chalk.grey(":") + chalk.yellow(message.content))
-    }
-});
+  if (message.content === '#log') {
+    log = !log;
+  } else if (log === true) {
+    console.log(chalk.green(message.guild.name) + chalk.grey(":") + logg(message.channel.name) + chalk.grey(":") + chalk.blue(message.author.tag) + chalk.grey(":") + chalk.yellow(message.content))
+  }
 
-client.on('message', message=> {
-   if (message.content === '#logon') {
+  if (message.content === '#logon') {
     client.user.setStatus('online');
-   }
-});
-
-client.on('message', message=> {
-   if (message.content === '#logoff') {
+  } else if (message.content === '#logoff') {
     client.user.setStatus('invisible');
-   }
-});
-
-client.on('message', message=> {
-    if (message.content === '#official') {
-        message.channel.send('**OfficialServer: https://goo.gl/ueccYS**')
-        message.channel.send('**OfficialInvite: https://goo.gl/nTJBXo**')
-   }
-});
-
-client.on('message', message=> {
-    if (message.content === '#hello') {
-        message.channel.send('Hello Comrade')
-    }
+  } else if (message.content === '#official') {
+    message.channel.send('**OfficialServer: https://goo.gl/ueccYS**\n**OfficialInvite: https://goo.gl/nTJBXo**');
+  } else if (message.content === '#hello') {
+    message.channel.send('Hello Comrade')
+  }
 });
 //Login your bot
 client.login(token)
